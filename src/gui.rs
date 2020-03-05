6ff49030f8a7acc034639d85bcf46e3a64ccbce2
Michael Ilyin <milyin@gmail.com>
2020-03-05 23:35:06 +0300

    initial

---
+++ src/gui.rs
@@ -0,0 +1,27 @@
+use ggez::{Context, GameResult};
+use ggez::event::MouseButton;
+
+pub trait EventHandlerProxy {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+    fn draw(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+    fn mouse_button_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        _button: MouseButton,
+        _x: f32,
+        _y: f32,
+    ) {
+    }
+    fn mouse_button_up_event(
+        &mut self,
+        _ctx: &mut Context,
+        _button: MouseButton,
+        _x: f32,
+        _y: f32,
+    ) {
+    }
+}
